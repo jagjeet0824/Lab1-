c766a79fb2b6dd953eb4b0873ef986e6e4e41cea
jagjeet0824 <jagjeet0824@gmail.com>
2018-08-16 15:26:56 -0400

    Final

---
+++ lesson1/server.js
@@ -0,0 +1,8 @@
+var http = require( 'http' );
+http.createServer( function( request, response ) {
+   response.writeHead(200)
+   response.write('Hello World');
+   response.end();
+   
+}).listen( 3000 );
+console.log( 'Server running on port 3000 ' );
